e2574f82a47a36e088d5b0fddebd61eb6513beb6
sidhartha11 <sidhartha11@yahoo.com>
2018-03-28 11:40:14 -0400

    update

--- simple-parent/algorithmMod/src/main/scala/org/geo/scala/graph/sedgewick/GlobalTestRunner.scala
+++ simple-parent/algorithmMod/src/main/scala/org/geo/scala/graph/sedgewick/GlobalTestRunner.scala
@@ -13,18 +13,18 @@ import scala.collection.mutable
  * 1. Buffer used as an adjacency list
  * In this case the vertex map is still a "HashMap" but
  * the actual neighbor list is a linked list structure.
- * 
+ *
  * 2. HashMap used as an adjacency list
- * In this case the vertex map is still a "HashMap" but the 
- * actual neighbor list is a HashMap also. 
- * 
+ * In this case the vertex map is still a "HashMap" but the
+ * actual neighbor list is a HashMap also.
+ *
  * The type of test made is discriminated by using the import aliasing mechanism
  * above: BUF for buffer and MAP for HashMap
- * 
- * The source of input comes from a file on the file system, though it is possible 
+ *
+ * The source of input comes from a file on the file system, though it is possible
  * to read from the net:
  * @see org.geo.scala.graph.GlobalUtilities.downloadFile
- * 
+ *
  * DIRECTION:
  * The adjacency list(or map) can be either directed or undirected.
  * THis is controlled by a variable to the instantiation of the Graph object.
@@ -35,38 +35,106 @@ import scala.collection.mutable
  * An instantiation of the Graph Object is as follows:
  * val a = BUF.GraphUtilities.initializeGraph(GlobalUtilities.filename, GraphConstants.undirected)
  * So here, filename will contain the input data in the form of:
- * node1,node2,weight where node1 and node2 are two connected verticies and weight is an 
+ * node1,node2,weight where node1 and node2 are two connected verticies and weight is an
  * indicator of the cost of the edge between them. ( currently not being used in this implementation )
- * 
+ *
  * Two types of search are implemented:
  * 1. recursive DFS search
  * This search type is limited by the stack size and will throw an Error is the input exceeds
  * available stack space.
- * 
- * 2. non-recursive BFS search 
- * 
- * 
- * 
+ *
+ * 2. non-recursive BFS search
+ *
+ *
+ *
  */
 object GlobalTestRunner {
 
   /**
    * Test Files Used
-   *  princetonLarge ( over 7meg ) 
+   *  princetonLarge ( over 7meg )
    *  princetonMedium.txt ( 400K records )
-   *  city.txt  small file of different cities 
+   *  city.txt  small file of different cities
    */
-  def direction = GraphConstants.undirected
+  def direction = GraphConstants.directed
   // def nodeToAnalyze="Montclair NJ"
-  def nodeToAnalyze="997847"
+  def nodeToAnalyze = "997847"
   def showTrace = true
-  /** BUFFER BASED **/
-  
-    /** TEST DFS algorithm **/
+  def filename = "city_cycle.txt"
+  // def filename = "numbers.txt"
+
+  /** TEST Connected Components  algorithm **/
+  def testCycleBUForMAP = {
+    /** create a graph object **/
+    //  val a = BUF.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
+//    val a = MAP.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
+    val a = MAP.GraphUtilities.initializeGraph(filename, direction)
+
+    println("\nmaxDegree of a is %d".format(MAP.GraphUtilities.maxDegree(a)))
+    if (showTrace) {
+      println("\nprinting graph via printGraph")
+      a.printGraph
+      println("\nprint contents of  map")
+      for (v <- a.getGraph) {
+        println(v._1 + ":" + a.adj(v._1))
+      }
+    }
+
+    println("running cycle test")
+    val start = System.currentTimeMillis()
+
+    val cc = MAP.Cycle(a)
+    //  val cc = BUF.ConnectedComponents(a)
+    /** get the number of connected components **/
+    val m = cc.count
+    /**
+     * Create a map of Int,Queue associations where each Int
+     * is a pointer to a Queue of vertices representing
+     * connected components
+     */
+    val components = mutable.Map[Int, mutable.Queue[GraphVertex]]()
+
+    /**
+     * traverse thru all known vertices, locate the Integer
+     * representing their connected components id,
+     * if the queue for the component is not currently in the map,
+     * put it there.
+     * put the current vertex into the queue for that component,
+     */
+
+    for ((v, l) <- a.getGraph) {
+      val comp = components get (cc.iD(v))
+      if (comp == None) {
+        val queue: mutable.Queue[GraphVertex] = new mutable.Queue[GraphVertex]()
+        queue += v
+        components += (cc.iD(v) -> queue)
+      } else {
+        comp.get += v
+      }
+    }
+
+    /** scan thru the components Map and print out the contents **/
+    for ((c_id, q) <- components) {
+      print("component id:" + c_id + "\n    ")
+      for (queue_item <- q) {
+        print(queue_item + " : ")
+      }
+      println()
+    }
+
+    val end = System.currentTimeMillis()
+    println("elapsed:%d".format((end - start)))
+    
+    println("Cycle Detected:%b".format(cc.isCyclic))
+
+  }
+  /** TEST Connected Components  algorithm **/
   def testConnectedBUForMAP = {
     /** create a graph object **/
-//  val a = BUF.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
-    val a = MAP.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
+    //  val a = BUF.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
+//    val a = MAP.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
+    val a = MAP.GraphUtilities.initializeGraph(filename, direction)
+
     println("\nmaxDegree of a is %d".format(MAP.GraphUtilities.maxDegree(a)))
     if (showTrace) {
       println("\nprinting graph via printGraph")
@@ -79,18 +147,18 @@ object GlobalTestRunner {
 
     println("running connected components test")
     val start = System.currentTimeMillis()
-    
+
     val cc = MAP.ConnectedComponents(a)
-//  val cc = BUF.ConnectedComponents(a)
+    //  val cc = BUF.ConnectedComponents(a)
     /** get the number of connected components **/
     val m = cc.count
     /**
      * Create a map of Int,Queue associations where each Int
-     * is a pointer to a Queue of vertices representing 
+     * is a pointer to a Queue of vertices representing
      * connected components
      */
-    val components = mutable.Map[Int,mutable.Queue[GraphVertex]]()
-    
+    val components = mutable.Map[Int, mutable.Queue[GraphVertex]]()
+
     /**
      * traverse thru all known vertices, locate the Integer
      * representing their connected components id,
@@ -98,10 +166,10 @@ object GlobalTestRunner {
      * put it there.
      * put the current vertex into the queue for that component,
      */
-    
-    for ( (v,l) <- a.getGraph ) {
+
+    for ((v, l) <- a.getGraph) {
       val comp = components get (cc.iD(v))
-      if ( comp == None ) {
+      if (comp == None) {
         val queue: mutable.Queue[GraphVertex] = new mutable.Queue[GraphVertex]()
         queue += v
         components += (cc.iD(v) -> queue)
@@ -109,21 +177,23 @@ object GlobalTestRunner {
         comp.get += v
       }
     }
-    
+
     /** scan thru the components Map and print out the contents **/
-    for ( (c_id , q ) <- components ) {
+    for ((c_id, q) <- components) {
       print("component id:" + c_id + "\n    ")
-      for ( queue_item <- q ) {
+      for (queue_item <- q) {
         print(queue_item + " : ")
       }
       println()
     }
-    
+
     val end = System.currentTimeMillis()
     println("elapsed:%d".format((end - start)))
 
-
   }
+  
+  
+    /** BUFFER BASED **/
   /** TEST DFS algorithm **/
   def testDFSBUF = {
     /** create a graph object **/
@@ -200,9 +270,9 @@ object GlobalTestRunner {
       }
     }
   }
-  
-    /** MAPPED BASED **/
-    /** TEST DFS algorithm **/
+
+  /** MAPPED BASED **/
+  /** TEST DFS algorithm **/
   def testDFSMAP = {
     /** create a graph object **/
     val a = MAP.GraphUtilities.initializeGraph(GlobalUtilities.filename, direction)
@@ -278,8 +348,8 @@ object GlobalTestRunner {
       }
     }
   }
-  
-    def testBFSMAPLarge(filename: String)  = {
+
+  def testBFSMAPLarge(filename: String) = {
     /** create a graph object **/
     val a = MAP.GraphUtilities.initializeGraph("C:\\temp\\", filename, direction)
     println("\nmaxDegree of a is %d".format(MAP.GraphUtilities.maxDegree(a)))
@@ -324,6 +394,7 @@ object GlobalTestRunner {
     // testBFSMAP
     // testDFSBUF
     // testBFSMAPLarge("princetonMedium.txt")
-    testConnectedBUForMAP
+    // testConnectedBUForMAP
+    testCycleBUForMAP
   }
 }
